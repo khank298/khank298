465e5450df2ca476a1dcdd2ba85a2b2259a20c15
Khushikhan <94075879+khank298@users.noreply.github.com>
2021-11-16 10:16:37 +0530

    Add files via upload

---
+++ AllOddNumbers1.java
@@ -0,0 +1,20 @@
+package AllOddNumbers;
+
+public class AllOddNumbers {
+
+  public static void main(String[] args) {
+  // print all odd numbers from 1 to 100
+		
+		for (int i = 1; i< 100; i=i+1) {
+			
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+			
+		}
+
+
+
+   
+  }
+}
